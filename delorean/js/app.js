1e481076b5560bae1e2b09e88c8aed12b6dad7c0
Marek Chowaniok <marek.chowaniok@gmail.com>
2015-10-19 13:38:33 +0200

    logout

--- delorean/js/app.js
+++ delorean/js/app.js
@@ -5,6 +5,8 @@ var Page = require("./components/Page");
 
 var Dispatcher = require("./dispatcher");
 
+console.log(Dispatcher);
+
 var rootElement = document.getElementById("app");
 
 React.render(<Page dispatcher={Dispatcher} />, rootElement);
